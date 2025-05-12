20d14242238351bb2b3c064fbb91f0038ee5a47b
ibra-qss <135248037+ibra860@users.noreply.github.com>
2025-05-12 11:11:58 +0300

    update_stream_node

--- src/camera_stream_node.cpp
+++ src/camera_stream_node.cpp
@@ -15,34 +15,41 @@ public:
         // Create an image publisher
         image_publisher_ = image_transport::create_publisher(this, "/camera/image_raw");
 
-        pointcloud_subscriber_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-            "/iv_points", 10, std::bind(&CameraStreamNode::callback, this, std::placeholders::_1));
+        /* It was used because Innovusion lidar counts time separately, so in 
+        * the system, lidar time is different than system time. For each 
+        * message published by lidar, we only take the header to get time 
+        * and set camera time with that time. */
+       
+        // pointcloud_subscriber_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+        //     "/iv_points", 10, std::bind(&CameraStreamNode::callback, this, std::placeholders::_1));
 
         // Open the camera (camera index 0 for default camera)
-        cap_.open("/dev/video42"); 
+        cap_.open("/dev/video0"); 
         if (!cap_.isOpened())
         {
             RCLCPP_ERROR(this->get_logger(), "Failed to open the camera.");
             rclcpp::shutdown();
             return;
         }
-
+        //May I stand unshaken? هل انت 
 
         RCLCPP_INFO(this->get_logger(), "Camera stream node started.");
 
         // Set a timer to periodically capture and publish frames
         timer_ = this->create_wall_timer(
-            std::chrono::milliseconds(100), // Approximately 30 FPS
+            std::chrono::milliseconds(50), // Approximately 30 FPS
             std::bind(&CameraStreamNode::publishFrame, this));
     }
 
 private:
 
-    void callback(sensor_msgs::msg::PointCloud2::ConstSharedPtr point_msg){
+    //save lidar time to set camera time
 
-        lidar_time_ = point_msg->header.stamp;
+    // void callback(sensor_msgs::msg::PointCloud2::ConstSharedPtr point_msg){
 
-    }
+    //     lidar_time_ = point_msg->header.stamp;
+
+    // }
 
     void publishFrame()
     {
@@ -57,7 +64,7 @@ private:
 
         // Convert the frame to a ROS Image message
         auto msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", frame).toImageMsg();
-        msg->header.stamp = lidar_time_;
+        // msg->header.stamp = now();
 
         // Publish the image
         image_publisher_.publish(msg);
@@ -66,8 +73,8 @@ private:
     image_transport::Publisher image_publisher_;
     cv::VideoCapture cap_;
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Time lidar_time_;
-    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_subscriber_;
+    // rclcpp::Time lidar_time_;
+    // rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_subscriber_;
 };
 
 int main(int argc, char *argv[])
